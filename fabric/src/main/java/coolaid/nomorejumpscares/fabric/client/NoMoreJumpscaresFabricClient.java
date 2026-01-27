513145433140d27d5ac9bf308947565b9ffff0e1
CoolAid48 <atgunsel@gmail.com>
2026-01-27 00:13:50 -0500

    Initial commit

---
+++ fabric/src/main/java/coolaid/nomorejumpscares/fabric/client/NoMoreJumpscaresFabricClient.java
@@ -0,0 +1,10 @@
+package coolaid.nomorejumpscares.fabric.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class NoMoreJumpscaresFabricClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+    }
+}
---
+++ neoforge/src/main/java/coolaid/nomorejumpscares/neoforge/NoMoreJumpscaresNeoForge.java
@@ -0,0 +1,12 @@
+package coolaid.nomorejumpscares.neoforge;
+
+import coolaid.nomorejumpscares.NoMoreJumpscares;
+import net.neoforged.fml.common.Mod;
+
+@Mod(NoMoreJumpscares.MOD_ID)
+public final class NoMoreJumpscaresNeoForge {
+    public NoMoreJumpscaresNeoForge() {
+        // Run our common setup.
+        NoMoreJumpscares.init();
+    }
+}
